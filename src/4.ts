45176eeb41e6840211453beb9f0535d64452204b
Viktoriia <128598090+kamineckav@users.noreply.github.com>
2024-02-21 12:19:30 +0100

    correction task-4

--- src/4.ts
+++ src/4.ts
@@ -1,5 +1,8 @@
 class Key {
-  private signature: number = Math.random() * 10;
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
 
   getSignature(): number {
     return this.signature;
@@ -7,32 +10,24 @@ class Key {
 }
 
 class Person {
-  constructor(private key: Key, public name: string) {
-    this.key = key;
-    this.name = name;
-  }
+  constructor(private key: Key) {}
 
   getKey(): Key {
     return this.key;
   }
-  getName(): string {
-    return this.name;
-  }
 }
 
 abstract class House {
+  protected tenants: Person[] = [];
   protected door: boolean = false;
-  protected key: Key;
-
-  constructor(key: Key) {
-    this.key = key;
-  }
+  constructor(protected key: Key) {}
 
   abstract openDoor(personKey: Key): void;
 
   comeIn(person: Person): void {
     if (this.door) {
-      console.log(`${person.name} entered the house.`);
+      this.tenants.push(person);
+      console.log(`Entered the house.`);
     } else {
       console.log("The door is closed. You cannot come in.");
     }
@@ -40,12 +35,9 @@ abstract class House {
 }
 
 class MyHouse extends House {
-  constructor(key: Key) {
-    super(key);
-  }
 
-  openDoor(personKey: Key): void {
-    if (personKey.getSignature() === this.key.getSignature()) {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
       console.log("The door is unlocked. You can come in!");
     } else {
@@ -55,10 +47,10 @@ class MyHouse extends House {
 }
 
 const key = new Key();
+const person = new Person(key);
 const house = new MyHouse(key);
-const person = new Person(key, "Bobo");
 
-house.openDoor(person.getKey());
+house.openDoor(key);
 house.comeIn(person);
 
 export {};
